faeb181b294be5a5a17c7350d213f1587553496e
Jorge <jorge_barcelino@hotmail.com>
2023-02-21 20:03:15 -0600

    first commit Jorge

--- src/utils/responses.handler.js
+++ src/utils/responses.handler.js
@@ -1,23 +1,23 @@
-const success = ({status, data, message, res}) => {
-    res.status(status).json({
-        error: false,
-        status: status,
-        message: message,
-        data: data
-    })
-} 
+const success = ({ status, data, message, res }) => {
+  res.status(status).json({
+    error: false,
+    status: status,
+    message: message,
+    data: data,
+  });
+};
 
-const error = ({status, data, message, res, fields}) => {
-    res.status(status).json({
-        error: true,
-        status: status,
-        message: message,
-        fields: fields,
-        data
-    })
-}
+const error = ({ status, err, message, res, fields }) => {
+  res.status(status).json({
+    error: true,
+    status: status,
+    message: message,
+    fields: fields,
+    data: err,
+  });
+};
 
 module.exports = {
-    success,
-    error
-}
+  success,
+  error,
+};
