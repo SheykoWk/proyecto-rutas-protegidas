692e3b0fa61ace3ff43e72ba9010537b93120094
Jorge <jorge_barcelino@hotmail.com>
2023-02-22 21:38:53 -0600

    fix test

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,25 +1,27 @@
-const { ExtractJwt, Strategy } = require('passport-jwt')
-const passport = require('passport')
-const {findUserById} = require('../users/users.controllers')
+const { ExtractJwt, Strategy } = require("passport-jwt");
+const passport = require("passport");
+const { findUserById } = require("../users/users.controllers");
+const { api: config } = require("../../config");
 
 const passportConfigs = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'academlo'
-}
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: config.jwtSecret,
+};
 
-passport.use(new Strategy(passportConfigs, (tokenDecoded, done) => {
+passport.use(
+  new Strategy(passportConfigs, (tokenDecoded, done) => {
     findUserById(tokenDecoded.id)
-    .then(data => {
-        if(data){
-            done(null, tokenDecoded)
+      .then((data) => {
+        if (data) {
+          done(null, tokenDecoded);
         } else {
-            done(null, false, 
-                {message: 'Token Incorrect'}
-            )}
-    })
-    .catch(err => {
-        done(err, false)
-    })
-}))
+          done(null, false, { message: "Token Incorrect" });
+        }
+      })
+      .catch((err) => {
+        done(err, false);
+      });
+  })
+);
 
-module.exports = passport.authenticate('jwt', {session: false})
+module.exports = passport.authenticate("jwt", { session: false });
