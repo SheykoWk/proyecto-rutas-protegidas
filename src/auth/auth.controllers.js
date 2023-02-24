a888d6c842dde1ef2a895c1be340e27cb70fbfc5
romimurua <99356832+romimurua@users.noreply.github.com>
2023-02-24 20:00:31 -0300

    Update auth.controllers.js

--- src/auth/auth.controllers.js
+++ src/auth/auth.controllers.js
@@ -1,7 +1,20 @@
-
+const { findUserByEmail } = require("../users/users.controllers");
+const { comparePassword } = require("../utils/crypto");
 
 const checkUsersCredentials = async (email, password) => {
+  try {
+    const user = await findUserByEmail(email);
 
-}
+    const isCorrectPass = comparePassword(password, user.password);
 
-module.exports = checkUsersCredentials
+    if (isCorrectPass) {
+      return user;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    return false;
+  }
+};
+
+module.exports = checkUsersCredentials;
